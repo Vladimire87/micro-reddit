c9ab37798b87148aaeb49c736274b9add87365bb
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-04-24 20:57:09 +0400

    model Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
+# Post
+# Validates
+# title [unique, present]
+# url [present]
+# user_id: [present]
+# belongs_to author
 class Post < ApplicationRecord
+  validates :title,
+            presence: true,
+            uniqueness: true
+  validates :url,
+            presence: true
+  validates :user_id,
+            presence: true
 end
