00293e9c1f1896ede53ad62427184123c7d8bd2b
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-04-24 23:18:57 +0400

    Comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,9 +4,7 @@ class Comment < ApplicationRecord
   validates :body,
             length: { maximum: 255,
                       too_long: '%<count>s characters is the maximum allowed' }
+  validates :user_id, :post_id, presence: true
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 end
-
-# // Ref: Comment.user_id > User.id
-# // Ref: Comment.post_id > Post.id
