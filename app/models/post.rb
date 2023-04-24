bbf103395c32ad848307e757383703655f916ef6
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-04-24 23:30:55 +0400

    finish

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,4 +15,5 @@ class Post < ApplicationRecord
   validates :user_id,
             presence: true
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
             presence: true,
             uniqueness: true
   has_many :posts
+  has_many :comments
 end
