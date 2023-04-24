2d525e012cf398660d407fd73d0d98b8d81b58ad
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-04-24 20:46:04 +0400

    User model validations , has_many :posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :username, :email, presence: true, uniqueness: true
-  validates :username, length: { minimum: 4, maximum: 12 }
+  validates :username,
+            presence: true,
+            uniqueness: true,
+            length: { minimum: 4, maximum: 12 }
+  validates :email,
+            presence: true,
+            uniqueness: true
+  has_many :posts
 end
-# Authors
-
-# username:string [unique, 4-12 chars, present]
-# email:string [unique, present]
-# id:integer
-# created_at:datetime
-# updated_at:datetime
-
-# has_many posts
-
-# Posts
-
-# title:string [unique, present]
-# body:text [present]
-# author_id:integer [present]
-# id:integer
-# created_at:datetime
-# updated_at:datetime
-
-# belongs_to author
